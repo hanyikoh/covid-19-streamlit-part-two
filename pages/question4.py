284bea745c392ae5126f91c1aa352b6fcf33c92d
hanyikoh <hanyikoh@gmail.com>
2021-11-01 12:22:23 +0000

    fiux

--- pages/question4.py
+++ pages/question4.py
@@ -14,8 +14,9 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVR
 from sklearn.linear_model import Lasso
 from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import LSTM,Dense,Dropout
+# from keras.models import Sequential
+# from keras.layers import LSTM,Dense,Dropout
+from PIL import Image
 import colorcet as cc
 
 r_naught_df = pd.read_csv("dataset/r-naught-value - All.csv")
@@ -45,63 +46,63 @@ def model_evaluation(model, X, y):
     r2 = results.mean()
     return mae, mse, r2
 
-def create_LSTM_model(X_train):
-  regressor = Sequential()
-  regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
-  regressor.add(Dropout(rate = 0.2))
-  regressor.add(LSTM(units = 150, return_sequences = False, input_shape = (X_train.shape[1], 1)))
-  regressor.add(Dropout(rate = 0.2))
-  regressor.add(Dense(1))
+# def create_LSTM_model(X_train):
+#   regressor = Sequential()
+#   regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+#   regressor.add(Dropout(rate = 0.2))
+#   regressor.add(LSTM(units = 150, return_sequences = False, input_shape = (X_train.shape[1], 1)))
+#   regressor.add(Dropout(rate = 0.2))
+#   regressor.add(Dense(1))
 
-  return regressor
+#   return regressor
 
-def Run_LSTM(training_set, df, is_cases):
-  sc = MinMaxScaler(feature_range = (0, 1))
-  #fit: get min/max of train data
-  training_set_scaled = sc.fit_transform(training_set)
-  # training_set_scaled = training_set
+# def Run_LSTM(training_set, df, is_cases):
+#   sc = MinMaxScaler(feature_range = (0, 1))
+#   #fit: get min/max of train data
+#   training_set_scaled = sc.fit_transform(training_set)
+#   # training_set_scaled = training_set
 
-  ## 30 timesteps and 1 output
-  X_train = []
-  y_train = []
-  for i in range(30, len(training_set_scaled)):
-      X_train.append(training_set_scaled[i-30: i, 0])
-      y_train.append(training_set_scaled[i, 0])
+#   ## 30 timesteps and 1 output
+#   X_train = []
+#   y_train = []
+#   for i in range(30, len(training_set_scaled)):
+#       X_train.append(training_set_scaled[i-30: i, 0])
+#       y_train.append(training_set_scaled[i, 0])
 
-  X_train, y_train = np.array(X_train), np.array(y_train)
+#   X_train, y_train = np.array(X_train), np.array(y_train)
 
-  X_train = np.reshape(X_train, newshape = (X_train.shape[0], X_train.shape[1], 1))
-  regressor = create_LSTM_model(X_train)
-  regressor.compile(loss='mean_squared_error', optimizer='adam')
-  regressor.fit(X_train, y_train, epochs=100, batch_size=32, verbose=2)
+#   X_train = np.reshape(X_train, newshape = (X_train.shape[0], X_train.shape[1], 1))
+#   regressor = create_LSTM_model(X_train)
+#   regressor.compile(loss='mean_squared_error', optimizer='adam')
+#   regressor.fit(X_train, y_train, epochs=100, batch_size=32, verbose=2)
   
-  dataset_test = df.iloc[-30:, :]
-  dataset_train = df.iloc[:-30, :]
-  if is_cases:
-    dataset_total = pd.concat((dataset_train['cases_new'], dataset_test['cases_new']),axis = 0)
-  else:
-    dataset_total = pd.concat((dataset_train['deaths_new'], dataset_test['deaths_new']),axis = 0)
-  inputs = dataset_total[len(dataset_total) - len(dataset_test) - 30:].values
-  inputs = inputs.reshape(-1, 1)
-  inputs = inputs.astype(float)
-  inputs = sc.transform(inputs)
-  # your codes
-  ## 60 timesteps and 1 output
-  X_test = []
-  y_test = []
-  for i in range(30, len(inputs)):
-      X_test.append(inputs[i-30: i, 0])
-      y_test.append(inputs[i, 0])
+#   dataset_test = df.iloc[-30:, :]
+#   dataset_train = df.iloc[:-30, :]
+#   if is_cases:
+#     dataset_total = pd.concat((dataset_train['cases_new'], dataset_test['cases_new']),axis = 0)
+#   else:
+#     dataset_total = pd.concat((dataset_train['deaths_new'], dataset_test['deaths_new']),axis = 0)
+#   inputs = dataset_total[len(dataset_total) - len(dataset_test) - 30:].values
+#   inputs = inputs.reshape(-1, 1)
+#   inputs = inputs.astype(float)
+#   inputs = sc.transform(inputs)
+#   # your codes
+#   ## 60 timesteps and 1 output
+#   X_test = []
+#   y_test = []
+#   for i in range(30, len(inputs)):
+#       X_test.append(inputs[i-30: i, 0])
+#       y_test.append(inputs[i, 0])
 
-  X_test, y_test = np.array(X_test), np.array(y_test)
-  X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-  y_test = np.reshape(y_test, (y_test.shape[0], 1))
-  predict_value = regressor.predict(X_test)
-  #inverse the scaled value
-  # your codes
-  predicted_values = sc.inverse_transform(predict_value)
-  real_values= sc.inverse_transform(y_test)
-  return predicted_values,real_values
+#   X_test, y_test = np.array(X_test), np.array(y_test)
+#   X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+#   y_test = np.reshape(y_test, (y_test.shape[0], 1))
+#   predict_value = regressor.predict(X_test)
+#   #inverse the scaled value
+#   # your codes
+#   predicted_values = sc.inverse_transform(predict_value)
+#   real_values= sc.inverse_transform(y_test)
+#   return predicted_values,real_values
 
 def app():
     st.markdown('>  Predict the R naught index of Malaysia and states')
@@ -143,27 +144,33 @@ def app():
     st.table(pd.DataFrame(data=d))
 
     ##visualize the prediction and real price
-    training_set = malaysia_case_df.iloc[:-30, 1:2].values
-    predicted_values, real_values = Run_LSTM(training_set,malaysia_case_df, True)
-    plt.plot(real_values, color = 'red', label = 'Real New Case Value')
-    plt.plot(predicted_values, color = 'blue', label = 'Predicted New Case Value')
+    # training_set = malaysia_case_df.iloc[:-30, 1:2].values
+    # predicted_values, real_values = Run_LSTM(training_set,malaysia_case_df, True)
+    # plt.plot(real_values, color = 'red', label = 'Real New Case Value')
+    # plt.plot(predicted_values, color = 'blue', label = 'Predicted New Case Value')
 
-    plt.title('Malaysia New Case Prediction')
-    plt.xlabel('Time')
-    plt.ylabel('New Case Number')
-    plt.legend()
-    plt.show()
-    st.pyplot()
+    # plt.title('Malaysia New Case Prediction')
+    # plt.xlabel('Time')
+    # plt.ylabel('New Case Number')
+    # plt.legend()
+    # plt.show()
+    # st.pyplot()
 
-    training_set = malaysia_death_df.iloc[:-30, 1:2].values
-    predicted_values, real_values = Run_LSTM(training_set,malaysia_death_df, False)
-    plt.plot(real_values, color = 'red', label = 'Real New Deaths Value')
-    plt.plot(predicted_values, color = 'blue', label = 'Predicted New Deaths Value')
-    # plt.ylim(np.amin(np.concatenate([predicted_values, real_values])),np.amax(np.concatenate([predicted_values, real_values])))
+    # training_set = malaysia_death_df.iloc[:-30, 1:2].values
+    # predicted_values, real_values = Run_LSTM(training_set,malaysia_death_df, False)
+    # plt.plot(real_values, color = 'red', label = 'Real New Deaths Value')
+    # plt.plot(predicted_values, color = 'blue', label = 'Predicted New Deaths Value')
+    # # plt.ylim(np.amin(np.concatenate([predicted_values, real_values])),np.amax(np.concatenate([predicted_values, real_values])))
 
-    plt.title('Malaysia New Deaths Prediction')
-    plt.xlabel('Time')
-    plt.ylabel('New Deaths Number')
-    plt.legend()
-    plt.show()
-    st.pyplot()
+    # plt.title('Malaysia New Deaths Prediction')
+    # plt.xlabel('Time')
+    # plt.ylabel('New Deaths Number')
+    # plt.legend()
+    # plt.show()
+    # st.pyplot()
+
+    image = Image.open('./new case trend prediction.png')
+    st.image(image, caption='/new case trends prediction',use_column_width=True)
+
+    image = Image.open('./new deaths trend prediction.png')
+    st.image(image, caption='/new deaths trends prediction',use_column_width=True)
