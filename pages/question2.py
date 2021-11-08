d174d441cd2851c7f758928bc252c962a868f9d6
Esther Tan <estherjq.wls@gmail.com>
2021-11-09 00:48:14 +0800

    Update question2.py

--- pages/question2.py
+++ pages/question2.py
@@ -79,16 +79,24 @@ def app():
     
     kmeans = KMeans(n_clusters=2, init='random')
     label = kmeans.fit_predict(vaccines_type_df)
-    vaccines_type_df['cluster_group'] = kmeans.labels_
-    vaccines_type_df['Date'] = vaccines_date_df['date'].values
-    figa = px.scatter(vaccines_type_df, x='Date', y='Pfizer', color='cluster_group')
-    figb = px.scatter(vaccines_type_df, x='Date', y='Sinovac', color='cluster_group')
-    figc = px.scatter(vaccines_type_df, x='Date', y='AstraZeneca', color='cluster_group')
-    figd = px.scatter(vaccines_type_df, x='Date', y='Cansino', color='cluster_group')
-    fig3 = go.Figure(data=figa.data + figb.data + figc.data + figd.data)
-    fig3.update_layout(xaxis_title="Date")
-    fig3.show()
-    st.plotly_chart(fig3, use_container_width=True)
+    u_labels = np.unique(label)
+    plt.figure(figsize=(15,10))
+    for i in u_labels:
+        plt.scatter(vaccines_type_df.iloc[label == i , 0] , vaccines_type_df.iloc[label == i , 1] , label = i)
+    plt.title('Daily Total Vaccine Doses in Malaysia')
+    plt.legend()
+    plt.show()
+    st.pyplot()
+    #vaccines_type_df['cluster_group'] = kmeans.labels_
+    #vaccines_type_df['Date'] = vaccines_date_df['date'].values
+    #figa = px.scatter(vaccines_type_df, x='Date', y='Pfizer', color='cluster_group')
+    #figb = px.scatter(vaccines_type_df, x='Date', y='Sinovac', color='cluster_group')
+    #figc = px.scatter(vaccines_type_df, x='Date', y='AstraZeneca', color='cluster_group')
+    #figd = px.scatter(vaccines_type_df, x='Date', y='Cansino', color='cluster_group')
+    #fig3 = go.Figure(data=figa.data + figb.data + figc.data + figd.data)
+    #fig3.update_layout(xaxis_title="Date")
+    #fig3.show()
+    #st.plotly_chart(fig3, use_container_width=True)
     
     st.markdown('By applying the K-means clustering algorithm, the four types of vaccines with different total doses over the 3 months are classified into two groups.\nOne group is the group with higher total doses, another group is the lower total doses group. We can observe that there are some vaccines having lower total doses compared to other vaccines, although it is already September 2021.\nThis could be indicating a supply and demand problem for specific type of vaccine. The reasons behind it could be the differences in the vaccines\' popularity and publicity effect. \nFor example, there were more people talking good about Pfizer and the government recently used Pfizer the most for vaccination whereas AstraZeneca was said to be more likely to cause side effects and blood clots, so less people wanted to take it.')
     st.title('Affinity Propagation')
@@ -110,4 +118,4 @@ def app():
     fig5.show()
     st.plotly_chart(fig5, use_container_width=True)
     
-    st.markdown("By applying affinity propagation algorithm on the cummulative vaccine doses and daily cases in Malaysia over the three months, the estimated number of clusters is 6. With the graph shown above is a concave shape of points plotted, we can induced that the six clusters are: \n1. Very low cummulative vaccine doses & very low daily cases (Purple)\n2. Low cummulative vaccine doses & low daily cases (Blue, light blue)\n3. Moderate cummulative vaccine doses & moderate daily cases (Aqua blue)\n4. High cummulative vaccine doses & very high daily cases (Green)\n5. High cummulative vaccine doses & moderate daily cases (Yellow, orange)\n6. Very high cummulative vaccine doses & low daily cases (Red)\nThe first cluster indicated a pandemic situation where the daily cases remained low and steady although the cummulative vaccine doses is not high. The second, third and fourth cluster indicated the climbing trend of both cummulative vaccine doses and daily cases, which means the vaccines effects have not shown yet and there might be outbreaks of COVID-19 happening at that time. The fifth cluster indicated the start of the daily cases decline trend where the vaccine can be said to be effective and have suppressed the pandemic a bit. The sixth cluster indicated the drastic drop of daily cases from its peak as the cummulative vaccine doses is very high.\nIn short, the vaccines are effective but require sometime to show its affects. Hence, we should continue the vaccinations.")
+    st.markdown("By applying affinity propagation algorithm on the cummulative vaccine doses and daily cases in Malaysia over the three months, the estimated number of clusters is 6. With the graph shown above is a concave shape of points plotted, we can induced that the six clusters are: \n1. Very low cummulative vaccine doses & very low daily cases (Blue)\n2. Low cummulative vaccine doses & low daily cases (Purple, violet)\n3. Moderate cummulative vaccine doses & moderate daily cases (Pink)\n4. High cummulative vaccine doses & very high daily cases (Orange)\n5. High cummulative vaccine doses & moderate daily cases (Orange yellow)\n6. Very high cummulative vaccine doses & low daily cases (Yellow)\nThe first cluster indicated a pandemic situation where the daily cases remained low and steady although the cummulative vaccine doses is not high. The second, third and fourth cluster indicated the climbing trend of both cummulative vaccine doses and daily cases, which means the vaccines effects have not shown yet and there might be outbreaks of COVID-19 happening at that time. The fifth cluster indicated the start of the daily cases decline trend where the vaccine can be said to be effective and have suppressed the pandemic a bit. The sixth cluster indicated the drastic drop of daily cases from its peak as the cummulative vaccine doses is very high.\nIn short, the vaccines are effective but require sometime to show its affects. Hence, we should continue the vaccinations.")
